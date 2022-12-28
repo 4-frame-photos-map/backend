c17e6c9a7563ddc9ce6f02e7a3934db89c91264f
choi seung keun <tmdrms0705@gmail.com>
2022-12-28 12:06:58 +0900

    Feat: 프로젝트 세팅
    - bulid 파일 추가 및 프로젝트 생성

---
+++ src/main/java/com/idea5/four_cut_photos_map/FourCutPhotosMapApplication.java
@@ -0,0 +1,13 @@
+package com.idea5.four_cut_photos_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourCutPhotosMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourCutPhotosMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idea5/four_cut_photos_map/FourCutPhotosMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea5.four_cut_photos_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourCutPhotosMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
