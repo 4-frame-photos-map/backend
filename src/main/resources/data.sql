98ec32dd25be0c7b9e65dcb01707723f289df923
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2023-01-09 02:40:29 +0900

    chore : 초기 데이터를 위한 data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into shop (create_date, modify_date, address, latitude, longitude, name) values (NOW(), NOW(), '인생네컷', 12.3, 15.2, '서울 강남구 ~');
+insert into shop (create_date, modify_date, address, latitude, longitude, name) values (NOW(), NOW(), '하루필름', 13.23, 18.2, '서울 강남구 ~');
+insert into shop (create_date, modify_date, address, latitude, longitude, name) values (NOW(), NOW(), '포토이즘', 19.23, 11.32, '서울 강남구 ~');
