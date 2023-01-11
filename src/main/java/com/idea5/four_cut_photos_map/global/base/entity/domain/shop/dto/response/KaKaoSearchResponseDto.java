e5a38cf4c4a6c04d339bb143b171df2e032920e7
Ju Minji <alswl0w0@gmail.com>
2023-01-11 22:16:15 +0900

    #8 feat : KakaoResponse DTO 추가

---
+++ src/main/java/com/idea5/four_cut_photos_map/global/base/entity/domain/shop/dto/response/KaKaoSearchResponseDto.java
@@ -0,0 +1,16 @@
+package com.idea5.four_cut_photos_map.global.base.entity.domain.shop.dto.response;
+
+import lombok.Data;
+
+public class KaKaoSearchResponseDto {
+    public Document[] documents;
+
+    @Data
+    static class Document {
+        public String place_name; // 장소명
+        public String road_address_name; // 도로명 주소
+        public String x; // 경도
+        public String y; // 위도
+        public String distance; // 중심좌표까지의 거리 (단, x,y 파라미터를 준 경우에만 존재)
+    }
+}
