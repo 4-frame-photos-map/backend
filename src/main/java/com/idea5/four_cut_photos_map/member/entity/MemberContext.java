1ec18178527925d138671df8610c9b4aa077d607
ahah525 <ahah525@naver.com>
2023-01-17 00:14:40 +0900

    #11 feat: Spring Security 사용자 정보를 담기위해 User를 상속한 MemberContext 추가

---
+++ src/main/java/com/idea5/four_cut_photos_map/member/entity/MemberContext.java
@@ -0,0 +1,33 @@
+package com.idea5.four_cut_photos_map.member.entity;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Spring Security 가 제공하는 로그인한 사용자 정보 커스텀 클래스
+ * @See <a href="https://sol-devlog.tistory.com/3"></>
+ */
+@Getter
+public class MemberContext extends User {
+    private final Long id;
+    private final LocalDateTime createDate;
+    private final LocalDateTime modifyDate;
+    private final String nickname;
+    private final Set<GrantedAuthority> authorities;
+
+    public MemberContext(Member member) {
+        // username, password, authorities
+        super(member.getId().toString(), "", member.getAuthorities());
+
+        this.id = member.getId();
+        this.createDate = member.getCreateDate();
+        this.modifyDate = member.getModifyDate();
+        this.nickname = member.getNickname();
+        this.authorities = member.getAuthorities().stream().collect(Collectors.toSet());
+    }
+}
