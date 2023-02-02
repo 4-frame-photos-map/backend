cb18ed1643af0c5b9e7cf962795be148615f4d8f
ahah525 <ahah525@naver.com>
2023-02-02 17:41:15 +0900

    #25 fix: Title, TitleLogs  양방향 연관관계 설정 및 불필요한 어노테이션 삭제

--- src/main/java/com/idea5/four_cut_photos_map/domain/title/entity/Title.java
+++ src/main/java/com/idea5/four_cut_photos_map/domain/title/entity/Title.java
@@ -7,8 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +23,6 @@ public class Title extends BaseEntity {
 
     private String name; // 칭호명
 
-    @OneToMany
-    @JoinColumn(name = "title_id")
+    @OneToMany(mappedBy = "title", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<TitleLog> titleLogs = new ArrayList<>();
-
-
 }
