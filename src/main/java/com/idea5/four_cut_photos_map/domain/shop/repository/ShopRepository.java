a4db2f521358ed99714b337b2b925b9b06b7acb9
Ju Minji <alswl0w0@gmail.com>
2023-02-15 19:04:04 +0900

    #34 test : 초기데이터 생성

--- src/main/java/com/idea5/four_cut_photos_map/domain/shop/repository/ShopRepository.java
+++ src/main/java/com/idea5/four_cut_photos_map/domain/shop/repository/ShopRepository.java
@@ -2,7 +2,10 @@ package com.idea5.four_cut_photos_map.domain.shop.repository;
 
 import com.idea5.four_cut_photos_map.domain.shop.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +16,9 @@ public interface ShopRepository extends JpaRepository<Shop, Long> {
     Optional<Shop> findByPlaceName(String keyword);
 
     Optional<List<Shop>> findByBrand(String keyword);
+
+    @Transactional
+    @Modifying
+    @Query(value = "ALTER TABLE shop AUTO_INCREMENT = 1", nativeQuery = true)
+    void truncate(); // for test;
 }
--- src/test/java/com/idea5/four_cut_photos_map/domain/shop/service/SearchShopByKeywordTests.java
+++ src/test/java/com/idea5/four_cut_photos_map/domain/shop/service/SearchShopByKeywordTests.java
@@ -2,12 +2,14 @@ package com.idea5.four_cut_photos_map.domain.shop.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.idea5.four_cut_photos_map.domain.shop.dto.KakaoKeywordResponseDto;
+import com.idea5.four_cut_photos_map.domain.shop.entity.Shop;
+import com.idea5.four_cut_photos_map.domain.shop.repository.ShopRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,11 +17,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 @SpringBootTest
-@Transactional
 @ActiveProfiles("test")
 public class SearchShopByKeywordTests {
     @Autowired
     private ShopService shopService;
+    @Autowired
+    private ShopRepository shopRepository;
+
+    @BeforeEach
+    void beforeEach(){
+        clearData();
+        createSampleData();
+    }
+
+    public void createSampleData(){
+        shopRepository.save(new Shop("인생네컷", "인생네컷 홍대동교점", "서울 마포구 홍익로6길 21", 126.922894949096, 37.555493447252));
+        shopRepository.save(new Shop("하루필름", "하루필름 연남점", "서울 마포구 동교로46길 40", 126.926725005048, 37.5621542536479));
+        shopRepository.save(new Shop("포토이즘박스", "포토이즘박스 망원점", "서울 마포구 포은로 88", 126.905387792703, 37.5548860583225));
+    }
+
+    public void clearData(){
+        shopRepository.deleteAll();
+        shopRepository.truncate();
+    }
 
     @Test
     @DisplayName("카카오맵 API 호출하여 키워드로 장소 검색하기, 키워드 = 지역명 + 즉석사진")
