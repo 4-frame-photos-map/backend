6906c3a725d07d6cab2f6971cc8840e068174466
ahah525 <ahah525@naver.com>
2023-03-09 16:32:22 +0900

    #32 test: 회원 탈퇴 테스트 추가, 회원 탈퇴시 redis 에 jwtRefreshToken 삭제 안되는 문제 발견

--- src/test/java/com/idea5/four_cut_photos_map/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/idea5/four_cut_photos_map/domain/member/service/MemberServiceTest.java
@@ -6,6 +6,8 @@ import com.idea5.four_cut_photos_map.domain.member.entity.Member;
 import com.idea5.four_cut_photos_map.domain.member.repository.MemberRepository;
 import com.idea5.four_cut_photos_map.global.common.RedisDao;
 import com.idea5.four_cut_photos_map.global.util.DatabaseCleaner;
+import com.idea5.four_cut_photos_map.security.jwt.JwtService;
+import com.idea5.four_cut_photos_map.security.jwt.dto.response.JwtToken;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,6 +26,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemberServiceTest {
     @Autowired
     MemberService memberService;
+
+    @Autowired
+    JwtService jwtService;
 
     @Autowired
     MemberRepository memberRepository;
@@ -50,7 +55,7 @@ class MemberServiceTest {
                 new KakaoTokenResp("bearer", "kakao_access_token", 60, "kakao_refresh_token", 86400));
 
         // then
-        // DB 에 저장된 member 검증
+        // 1. DB 저장된 member 검증
         List<Member> members = memberRepository.findAll();
         assertThat(members.size()).isEqualTo(1);
 
@@ -60,11 +65,10 @@ class MemberServiceTest {
         assertThat(member.getNickname()).isEqualTo("딸기");
         assertThat(member.getKakaoRefreshToken()).isEqualTo("kakao_refresh_token");
 
-        // redis 에 저장된 nickname, kakaoAccessToken 검증
-        String nicknameKey = "member:" + member.getId() + ":nickname";
-        String kakaoAtkKey = "member:" + member.getId() + ":kakao_access_token";
-        assertThat(redisDao.getValues(nicknameKey)).isEqualTo("딸기");
-        assertThat(redisDao.getValues(kakaoAtkKey)).isEqualTo("kakao_access_token");
+        // 2. Redis 저장된 nickname, kakaoAccessToken 검증
+        assertThat(redisDao.getValues("member:" + member.getId() + ":nickname")).isEqualTo("딸기");
+        assertThat(redisDao.getValues("member:" + member.getId() + ":kakao_access_token"))
+                .isEqualTo("kakao_access_token");
     }
 
     @Test
@@ -82,7 +86,7 @@ class MemberServiceTest {
                 new KakaoTokenResp("bearer", "kakao_access_token2", 60, "kakao_refresh_token2", 86400));
 
         // then
-        // 1. DB 의 kakaoRefreshToken 값만 수정됬는지 검증
+        // 1. DB kakaoRefreshToken 값만 수정됬는지 검증
         List<Member> members = memberRepository.findAll();
         assertThat(members.size()).isEqualTo(1);
 
@@ -92,10 +96,32 @@ class MemberServiceTest {
         assertThat(member.getNickname()).isEqualTo("딸기");
         assertThat(member.getKakaoRefreshToken()).isEqualTo("kakao_refresh_token2");
 
-        // 2. redis 의 kakaoAccessToken 값만 수정됬는지 검증
-        String nicknameKey = "member:" + member.getId() + ":nickname";
-        String kakaoAtkKey = "member:" + member.getId() + ":kakao_access_token";
-        assertThat(redisDao.getValues(nicknameKey)).isEqualTo("딸기");
-        assertThat(redisDao.getValues(kakaoAtkKey)).isEqualTo("kakao_access_token2");
+        // 2. Redis kakaoAccessToken 값만 수정됬는지 검증
+        assertThat(redisDao.getValues("member:" + member.getId() + ":nickname")).isEqualTo("딸기");
+        assertThat(redisDao.getValues("member:" + member.getId() + ":kakao_access_token"))
+                .isEqualTo("kakao_access_token2");
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴시 DB 에서 삭제, Redis 의 refreshToken 삭제, accessToken 블랙리스트로 등록")
+    void t3() {
+        // given
+        Member member = memberService.getMember(
+                new KakaoUserInfoParam(1111L, "딸기"),
+                new KakaoTokenResp("bearer", "kakao_access_token", 60, "kakao_refresh_token", 86400));
+        JwtToken jwtToken = jwtService.generateTokens(member);
+        String key3 = "member:" + member.getId() + ":jwt_refresh_token";
+        assertThat(redisDao.getValues(key3)).isEqualTo(jwtToken.getRefreshToken());
+
+        // when
+        memberService.deleteMember(member.getId(), jwtToken.getAccessToken());
+
+        // then
+        // 1. DB Member 삭제
+        assertThat(memberRepository.count()).isEqualTo(0);
+
+        // 2. Redis jwtAccessToken 블랙리스트 등록, jwtRefreshToken 삭제
+        assertThat(redisDao.getValues("jwt_black_list:" + jwtToken.getAccessToken())).isEqualTo("withdrawl");
+        assertThat(redisDao.getValues("member:" + member.getId() + ":jwt_refresh_token")).isNull();
     }
 }
