fc51b808accabd24b6aae6ae6abee347b15b9492
ahah525 <ahah525@naver.com>
2023-03-29 11:41:52 +0900

    #60 test: 로그아웃/회원탈퇴시 accessToken 블랙리스트 저장 검증 코드 삭제

--- src/test/java/com/idea5/four_cut_photos_map/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/idea5/four_cut_photos_map/domain/member/service/MemberServiceTest.java
@@ -136,14 +136,15 @@ class MemberServiceTest {
         assertThat(redisDao.getValues(RedisDao.getRtkKey(member.getId()))).isEqualTo(jwtToken.getRefreshToken());
 
         // when
-        memberService.deleteMember(member.getId(), jwtToken.getAccessToken());
+        memberService.deleteMember(member.getId());
 
         // then
-        // 1. DB Member 삭제
-        assertThat(memberRepository.count()).isEqualTo(0);
-        // 2. Redis jwtAccessToken 블랙리스트 등록, jwtRefreshToken 삭제
-        assertThat(redisDao.getValues(RedisDao.getBlackListAtkKey(jwtToken.getAccessToken()))).isEqualTo("withdrawl");
-        assertThat(redisDao.getValues(RedisDao.getRtkKey(member.getId()))).isNull();
+        assertAll(
+                // 1. DB Member 삭제
+                () -> assertThat(memberRepository.count()).isEqualTo(0),
+                // 2. Redis jwtAccessToken 블랙리스트 등록, jwtRefreshToken 삭제
+                () -> assertThat(redisDao.getValues(RedisDao.getRtkKey(member.getId()))).isNull()
+        );
     }
 
     @Test
@@ -223,12 +224,11 @@ class MemberServiceTest {
         assertThat(redisDao.getValues(RedisDao.getRtkKey(member.getId()))).isEqualTo(jwtToken.getRefreshToken());
 
         // when
-        memberService.logout(member.getId(), jwtToken.getAccessToken());
+        memberService.logout(member.getId());
 
         // then
         // 1. Redis jwtAccessToken 블랙리스트 등록, jwtRefreshToken 삭제
         assertAll(
-                () -> assertThat(redisDao.getValues(RedisDao.getBlackListAtkKey(jwtToken.getAccessToken()))).isEqualTo("logout"),
                 () -> assertThat(redisDao.getValues(RedisDao.getRtkKey(member.getId()))).isNull()
         );
     }
