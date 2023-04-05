dbac5b77af7082e9313a60f22a579983320ca056
Ju Minji <alswl0w0@gmail.com>
2023-04-05 15:59:04 +0900

    #67 refactor : 대표 브랜드 올바르게 설정

--- src/main/java/com/idea5/four_cut_photos_map/global/common/data/Brand.java
+++ src/main/java/com/idea5/four_cut_photos_map/global/common/data/Brand.java
@@ -1,5 +1,5 @@
 package com.idea5.four_cut_photos_map.global.common.data;
 
 public class Brand {
-    public static String[] Names = {"하루필름", "인생네컷", "포토이즘박스", "픽닷"};
+    public static String[] Names = {"하루필름", "인생네컷", "포토이즘박스","포토이즘", "포토그레이"};
 }
