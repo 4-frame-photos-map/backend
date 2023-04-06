3ee2c6c49603265485e7e7cc68266c1fcd600f3e
ahah525 <ahah525@naver.com>
2023-04-06 19:33:55 +0900

    refactor: snake case 표기로 응답

--- src/main/java/com/idea5/four_cut_photos_map/security/jwt/dto/response/AccessToken.java
+++ src/main/java/com/idea5/four_cut_photos_map/security/jwt/dto/response/AccessToken.java
@@ -1,5 +1,7 @@
 package com.idea5.four_cut_photos_map.security.jwt.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AccessToken {
     private String accessToken;
 }
