7a2b8fee44d7e10daaa6780e3a1e138e0e02c939
Ju Minji <alswl0w0@gmail.com>
2023-04-12 00:08:33 +0900

    #72 feat : 상세조회 API에서 호출하는 카카오 API 응답값 Redis에 캐시
    - 도로명주소를 기준으로(Key) 장소명, 장소URL, 위도, 경도 캐시

--- src/main/java/com/idea5/four_cut_photos_map/domain/shop/service/kakao/KakaoMapSearchApi.java
+++ src/main/java/com/idea5/four_cut_photos_map/domain/shop/service/kakao/KakaoMapSearchApi.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.idea5.four_cut_photos_map.domain.shop.dto.response.KakaoMapSearchDto;
+import com.idea5.four_cut_photos_map.global.common.RedisDao;
 import com.idea5.four_cut_photos_map.global.util.Util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +29,7 @@ public class KakaoMapSearchApi {
     @Value("${oauth2.kakao.client-id}")
     private String SECOND_API_KEY;
     private final RestTemplate restTemplate;
+    private final RedisDao redisDao;
     private final ObjectMapper objectMapper;
     public final int radius= 2000;
     public static final String DEFAULT_QUERY_WORD = "즉석사진";
@@ -35,12 +38,11 @@ public class KakaoMapSearchApi {
     public List<KakaoMapSearchDto> searchByQueryWord(String queryWord, Double longitude, Double latitude, boolean hasRadius) {
         List<KakaoMapSearchDto> resultList = new ArrayList<>();
 
-        // 1. header 설정
+        // 1. API 호출을 위한 요청 설정
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "KakaoAK " + FIRST_API_KEY);
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        // 2. 요청 URL 정의
         String apiURL = "https://dapi.kakao.com/v2/local/search/keyword.JSON?"
                 + "query=" + queryWord + DEFAULT_QUERY_WORD
                 + "&x=" + longitude
@@ -50,27 +52,36 @@ public class KakaoMapSearchApi {
                 += "&sort=distance" // 거리순 정렬
                 + "&radius=" + radius; // 반경 2km 이내
 
-        // 3. api 호출
+        // 2. API 호출
         JsonNode documents = restTemplate.exchange(apiURL, HttpMethod.GET, entity, JsonNode.class)
                 .getBody()
                 .get("documents");
 
-        // 4. JSON -> DTO 역직렬화
+        // 3. JSON -> DTO 역직렬화
         return deserialize(resultList, documents);
     }
 
     public String[] searchByRoadAddressName(String roadAddressName) {
-        // 1. header 설정
+        // 1. Redis에서 조회
+        String cacheKey = redisDao.getRoadAddressKey(roadAddressName);
+        String cachedData = redisDao.getValues(cacheKey);
+
+        if (cachedData != null) {
+            log.info("=====RoadAddressName Cache Hit=====");
+            return cachedData.split(",");
+        }
+        log.info("=====RoadAddressName Cache Miss=====");
+
+        // 2. API 호출을 위한 요청 설정
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "KakaoAK " + FIRST_API_KEY);
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        // 2. 요청 URL 정의
         String apiURL = "https://dapi.kakao.com/v2/local/search/keyword.JSON?"
                 + "query=" + roadAddressName + DEFAULT_QUERY_WORD
                 + "&size=1"; // 정확도순 상위 하나의 지점만 응답받도록 제한
 
-        // 3. api 호출
+        // 3. API 호출
         JsonNode document = restTemplate.exchange(apiURL, HttpMethod.GET, entity, JsonNode.class)
                 .getBody()
                 .get("documents")
@@ -79,34 +90,40 @@ public class KakaoMapSearchApi {
         // 4. JSON -> String 역직렬화
         // 100% 일치 결과 없으면 유사도 제일 높은 장소 받아오기 때문에
         // 요청 도로명 주소와 완전히 일치하는지 검사 필요
-        if(document.get("road_address_name").asText().equals(roadAddressName))
-            return new String[] {
+        if(document.get("road_address_name").asText().equals(roadAddressName)){
+            String[] result = new String[] {
                     document.get("place_name").asText(), document.get("place_url").asText(),
                     document.get("x").asText(), document.get("y").asText()
-        };
-        else return null;
+            };
+
+            // 5. Redis에 데이터 저장
+            redisDao.setValues(cacheKey, String.join(",", result), Duration.ofDays(5));
+
+            return result;
+        } else {
+            return null;
+        }
     }
 
     public String[] searchByRoadAddressName(String roadAddressName, Double curLnt, Double curLat) {
-        // 1. header 설정
+        // 1. API 호출을 위한 요청 설정
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "KakaoAK " + SECOND_API_KEY);
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        // 2. 요청 URL 정의
         String apiURL = "https://dapi.kakao.com/v2/local/search/keyword.JSON?"
                 + "query=" + roadAddressName + DEFAULT_QUERY_WORD
                 + "&x=" + curLnt
                 + "&y=" + curLat
                 + "&size=1"; // 정확도순 상위 하나의 지점만 응답받도록 제한
 
-        // 3. api 호출
+        // 2. API 호출
         JsonNode document = restTemplate.exchange(apiURL, HttpMethod.GET, entity, JsonNode.class)
                 .getBody()
                 .get("documents")
                 .get(0);
 
-        // 4. JSON -> String 역직렬화
+        // 3. JSON -> String 역직렬화
         // 100% 일치 결과 없으면 유사도 제일 높은 장소 받아오기 때문에
         // 요청 도로명 주소와 완전히 일치하는지 검사 필요
         if(document.get("road_address_name").asText().equals(roadAddressName))
