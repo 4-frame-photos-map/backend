defff51be589b1b6f2c43dd0ab519cdd32c02932
ahah525 <ahah525@naver.com>
2023-04-11 23:50:52 +0900

    #11 feat: 파일 업로드 API 호출시 s3 객체 URL 응답

--- src/main/java/com/idea5/four_cut_photos_map/domain/file/controller/FileUploadController.java
+++ src/main/java/com/idea5/four_cut_photos_map/domain/file/controller/FileUploadController.java
@@ -2,6 +2,7 @@ package com.idea5.four_cut_photos_map.domain.file.controller;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.idea5.four_cut_photos_map.domain.file.dto.response.UploadImageResp;
 import com.idea5.four_cut_photos_map.global.error.ErrorCode;
 import com.idea5.four_cut_photos_map.global.error.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
@@ -29,7 +30,7 @@ public class FileUploadController {
     private String bucket;
 
     @PostMapping
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<UploadImageResp> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
             // 1. 이미지 파일이 아닌 경우 예외처리
             if(file.getContentType().startsWith("image") == false) {
@@ -37,12 +38,16 @@ public class FileUploadController {
                 throw new BusinessException(ErrorCode.NOT_IMAGE_FILE);
             }
             String fileName = file.getOriginalFilename();
+            System.out.println(fileName);
             String fileUrl = "https://" + bucket + "/test" + fileName;
             ObjectMetadata metadata= new ObjectMetadata();
             metadata.setContentType(file.getContentType());
             metadata.setContentLength(file.getSize());
+            // s3 파일 업로드
             amazonS3Client.putObject(bucket,fileName, file.getInputStream(), metadata);
-            return ResponseEntity.ok(fileUrl);
+            // s3 객체 URL 조회
+            String uploadImageUrl = amazonS3Client.getUrl(bucket, fileName).toString();
+            return ResponseEntity.ok(new UploadImageResp(uploadImageUrl));
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
