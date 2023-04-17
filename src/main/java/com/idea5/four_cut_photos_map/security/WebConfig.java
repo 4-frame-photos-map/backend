d73a536a649c1bbead8cd9f78d34abe4548adc0b
ahah525 <ahah525@naver.com>
2023-04-17 11:07:11 +0900

    #92 feat: 프론트 도메인 orgin 허용 설정

--- src/main/java/com/idea5/four_cut_photos_map/security/WebConfig.java
+++ src/main/java/com/idea5/four_cut_photos_map/security/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://photosmap.vercel.app")
                 .allowedMethods("*")
                 .allowCredentials(true)
                 .maxAge(3000);
