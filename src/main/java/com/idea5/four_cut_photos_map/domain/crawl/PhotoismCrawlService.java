7769ab11a0e426915a350fc5ecd4f1d4c9bf359b
ahah525 <ahah525@naver.com>
2023-05-01 20:46:07 +0900

    #114 feat: 포토이즘박스, 포토이즘컬러드 띄어쓰기 삭제

--- src/main/java/com/idea5/four_cut_photos_map/domain/crawl/PhotoismCrawlService.java
+++ src/main/java/com/idea5/four_cut_photos_map/domain/crawl/PhotoismCrawlService.java
@@ -39,8 +39,11 @@ public class PhotoismCrawlService implements CrawlService {
                     Elements titles = document.select("div.map_container.clearfix.map-inner._map_container");
 
                     for (Element e : titles) {
-                        String placeName = e.select("div.tit").text().trim();
+                        String placeName = e.select("div.tit").text()
+                                .replace("포토이즘 박스", "포토이즘박스")
+                                .replace("포토이즘 컬러드", "포토이즘컬러드").trim();
                         String address = e.select("p.adress").text().trim();
+
                         // 지점명으로 중복 검사
                         if (shopRepository.existsByPlaceName(placeName)) continue;
                         Shop shop = Shop.builder()
