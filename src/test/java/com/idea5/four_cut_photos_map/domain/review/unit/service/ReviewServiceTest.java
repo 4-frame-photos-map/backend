01fae2293ae101aa8a9fa6e85c6ec814aa9f1f15
choi seung keun <tmdrms0705@gmail.com>
2023-05-03 21:19:50 +0900

    test : 단일 리뷰 검색 테스트 수정

--- src/test/java/com/idea5/four_cut_photos_map/domain/review/unit/service/ReviewServiceTest.java
+++ src/test/java/com/idea5/four_cut_photos_map/domain/review/unit/service/ReviewServiceTest.java
@@ -3,7 +3,10 @@ package com.idea5.four_cut_photos_map.domain.review.unit.service;
 import com.idea5.four_cut_photos_map.domain.brand.entity.Brand;
 import com.idea5.four_cut_photos_map.domain.brand.entity.MajorBrand;
 import com.idea5.four_cut_photos_map.domain.member.entity.Member;
+import com.idea5.four_cut_photos_map.domain.review.dto.request.RequestReviewDto;
+import com.idea5.four_cut_photos_map.domain.review.dto.response.ResponseMemberReviewDto;
 import com.idea5.four_cut_photos_map.domain.review.dto.response.ResponseReviewDto;
+import com.idea5.four_cut_photos_map.domain.review.dto.response.ResponseShopReviewDto;
 import com.idea5.four_cut_photos_map.domain.review.entity.Review;
 import com.idea5.four_cut_photos_map.domain.review.entity.score.ItemScore;
 import com.idea5.four_cut_photos_map.domain.review.entity.score.PurityScore;
@@ -11,6 +14,8 @@ import com.idea5.four_cut_photos_map.domain.review.entity.score.RetouchScore;
 import com.idea5.four_cut_photos_map.domain.review.repository.ReviewRepository;
 import com.idea5.four_cut_photos_map.domain.review.service.ReviewService;
 import com.idea5.four_cut_photos_map.domain.shop.entity.Shop;
+import com.idea5.four_cut_photos_map.domain.shop.repository.ShopRepository;
+import com.idea5.four_cut_photos_map.domain.shop.service.ShopService;
 import com.idea5.four_cut_photos_map.global.error.ErrorCode;
 import com.idea5.four_cut_photos_map.global.error.exception.BusinessException;
 import org.junit.jupiter.api.*;
@@ -21,6 +26,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
@@ -29,14 +36,27 @@ import static org.mockito.Mockito.when;
 public class ReviewServiceTest {
     @InjectMocks
     private ReviewService reviewService;
-
     @Mock
     private ReviewRepository reviewRepository;
-
+    @Mock
+    private ShopService shopService;
 
     @Nested
     @DisplayName("단일 리뷰 검색")
     class RetrieveSingleReview {
+        private Member writer;
+        private Brand brand;
+        private Shop shop;
+        private Review review;
+
+        @BeforeEach
+        void setUp() {
+            writer = Member.builder().id(1L).kakaoId(1000L).nickname("user1").build();
+            brand = Brand.builder().id(1L).brandName("인생네컷").filePath("https://d18tllc1sxg8cp.cloudfront.net/brand_image/brand_1.jpg").build();
+            shop = Shop.builder().id(1L).brand(brand).placeName("인생네컷망리단길점").address("서울 마포구 포은로 109-1").favoriteCnt(0).reviewCnt(0).starRatingAvg(0.0).build();
+            review = Review.builder().id(1L).createDate(LocalDateTime.now()).modifyDate(LocalDateTime.now()).writer(writer).shop(shop).starRating(5).content("리뷰 내용").purity(PurityScore.GOOD).retouch(RetouchScore.GOOD).item(ItemScore.GOOD).build();
+        }
+
         @Nested
         @DisplayName("성공")
         class SuccessCase {
@@ -44,28 +64,24 @@ public class ReviewServiceTest {
             @DisplayName("해당 id를 가진 리뷰 존재")
             void retrieveSingleReviewSuccess1() {
                 //given
-                Long reviewId = 1L;
-                String content = "dd";
-                Brand brand = Brand.builder().brandName("인생네컷").filePath("https://d18tllc1sxg8cp.cloudfront.net/brand_image/brand_1.jpg").build();
-                Member writer = Member.builder().id(1L).createDate(LocalDateTime.now()).modifyDate(LocalDateTime.now()).kakaoId(1000L).nickname("user1").build();
-                Shop shop = Shop.builder().id(1L).createDate(LocalDateTime.now()).modifyDate(LocalDateTime.now()).brand(brand).placeName("인생네컷망리단길점").address("서울 마포구 포은로 109-1").favoriteCnt(0).reviewCnt(0).starRatingAvg(0.0).build();
-                Review review = Review.builder().id(1L).createDate(LocalDateTime.now()).modifyDate(LocalDateTime.now()).writer(writer).shop(shop).starRating(5).content(content).purity(PurityScore.GOOD).retouch(RetouchScore.GOOD).item(ItemScore.GOOD).build();
+                Long retrieveReviewId = 1L;
 
                 // when
-                when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+                when(reviewRepository.findById(retrieveReviewId)).thenReturn(Optional.of(review));
 
-                ResponseReviewDto responseReviewDto = reviewService.getReviewById(reviewId);
+                ResponseReviewDto responseReviewDto = reviewService.getReviewById(retrieveReviewId);
 
                 // then
-                Assertions.assertEquals(responseReviewDto.getReviewInfo().getId(), reviewId);
-                Assertions.assertEquals(responseReviewDto.getReviewInfo().getContent(), content);
-
-                Assertions.assertEquals(responseReviewDto.getShopInfo().getId(), shop.getId());
-                Assertions.assertEquals(responseReviewDto.getShopInfo().getPlaceName(), shop.getPlaceName());
-                Assertions.assertEquals(responseReviewDto.getShopInfo().getBrand(), brand.getBrandName());
+                Assertions.assertEquals(responseReviewDto.getReviewInfo().getId(), review.getId());
+                Assertions.assertEquals(responseReviewDto.getReviewInfo().getStarRating(), review.getStarRating());
+                Assertions.assertEquals(responseReviewDto.getReviewInfo().getContent(), review.getContent());
 
                 Assertions.assertEquals(responseReviewDto.getMemberInfo().getId(), writer.getId());
                 Assertions.assertEquals(responseReviewDto.getMemberInfo().getNickname(), writer.getNickname());
+
+                Assertions.assertEquals(responseReviewDto.getShopInfo().getId(), shop.getId());
+                Assertions.assertEquals(responseReviewDto.getShopInfo().getBrand(), brand.getBrandName());
+                Assertions.assertEquals(responseReviewDto.getShopInfo().getPlaceName(), shop.getPlaceName());
             }
         }
 
@@ -76,15 +92,15 @@ public class ReviewServiceTest {
             @DisplayName("해당 id의 리뷰 존재하지 않음")
             void retrieveSingleReviewFail1() {
                 // given
-                Long reviewId = 1L;
+                Long retrieveReviewId = 2L;
+                BusinessException exception = new BusinessException(ErrorCode.REVIEW_NOT_FOUND);
 
                 // when
-                when(reviewRepository.findById(reviewId)).thenThrow(new BusinessException(ErrorCode.REVIEW_NOT_FOUND));
-
-                BusinessException e = Assertions.assertThrows(BusinessException.class, () -> reviewService.getReviewById(reviewId));
+                when(reviewRepository.findById(retrieveReviewId)).thenThrow(exception);
 
                 // then
-                Assertions.assertEquals(ErrorCode.REVIEW_NOT_FOUND.getMessage(), e.getMessage());
+                BusinessException resultException = Assertions.assertThrows(exception.getClass(), () -> reviewService.getReviewById(retrieveReviewId));
+                Assertions.assertEquals(resultException.getMessage(), exception.getMessage());
             }
         }
     }
