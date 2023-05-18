2c9b5fef1086661b31b0c03412a92f57458560cb
ahah525 <ahah525@naver.com>
2023-05-18 15:20:49 +0900

    #131 refactor: 회원이 보유한 칭호 단건 조회 쿼리 개선

--- src/main/java/com/idea5/four_cut_photos_map/domain/memberTitle/repository/MemberTitleLogRepository.java
+++ src/main/java/com/idea5/four_cut_photos_map/domain/memberTitle/repository/MemberTitleLogRepository.java
@@ -1,6 +1,7 @@
 package com.idea5.four_cut_photos_map.domain.memberTitle.repository;
 
 import com.idea5.four_cut_photos_map.domain.member.entity.Member;
+import com.idea5.four_cut_photos_map.domain.memberTitle.entity.MemberTitle;
 import com.idea5.four_cut_photos_map.domain.memberTitle.entity.MemberTitleLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,11 +13,7 @@ public interface MemberTitleLogRepository extends JpaRepository<MemberTitleLog, 
 
     List<MemberTitleLog> findByMember(Member member);
 
-    Optional<MemberTitleLog> findByMemberIdAndMemberTitleId(Long memberId, Long memberTitleId);
-
-    Optional<MemberTitleLog> findByMemberIdAndIsMainTrue(Long memberId);
-
     Optional<MemberTitleLog> findByMemberAndIsMainTrue(Member member);
 
-    Optional<MemberTitleLog> findByMemberAndMemberTitleId(Member member, Long memberTitleId);
+    Optional<MemberTitleLog> findByMemberAndMemberTitle(Member member, MemberTitle memberTitle);
 }
