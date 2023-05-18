b30452e145c6eb5092500621bdfced6ca9e43108
Ju Minji <alswl0w0@gmail.com>
2023-05-18 18:41:09 +0900

    #136 test : 지점 칭호 부여 기준 시간 테스트용으로 설정

--- src/main/java/com/idea5/four_cut_photos_map/job/ShopTitleCollectJob.java
+++ src/main/java/com/idea5/four_cut_photos_map/job/ShopTitleCollectJob.java
@@ -14,13 +14,13 @@ public class ShopTitleCollectJob {
     private final HotPlaceTitleService hotPlaceTitleService;
     private final GoodCleanlinessTitleService goodCleanlinessTitleService;
 //    @Scheduled(cron = "0 0 3 1 * *") // 매달 1일 3시에 실행
-    @Scheduled(cron = "0 40 * * * *")
+    @Scheduled(cron = "0 45 * * * *")
     public void collectShopTitle() {
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime lastMonthStart = now.withDayOfMonth(1).minusMonths(1);
         LocalDateTime lastMonthEnd = now.withDayOfMonth(1).minusDays(1);
 
-        hotPlaceTitleService.collectHotPlaceTitles(lastMonthStart, lastMonthEnd);
-        goodCleanlinessTitleService.collectGoodCleanlinessTitles(lastMonthStart, lastMonthEnd);
+        hotPlaceTitleService.collectHotPlaceTitles(lastMonthStart, now);
+        goodCleanlinessTitleService.collectGoodCleanlinessTitles(lastMonthStart, now);
     }
 }
