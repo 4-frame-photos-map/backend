a80ed573d1cc42fb4c9644d9b1948a0815e776b9
ahah525 <ahah525@naver.com>
2023-05-18 14:55:02 +0900

    #139 test: 획득하고 대표 칭호로 설정되지 않은 칭호 정보 조회 테스트 코드 작성

--- src/test/java/com/idea5/four_cut_photos_map/domain/memberTitle/service/MemberTitleServiceTest.java
+++ src/test/java/com/idea5/four_cut_photos_map/domain/memberTitle/service/MemberTitleServiceTest.java
@@ -5,8 +5,12 @@ import com.idea5.four_cut_photos_map.domain.member.repository.MemberRepository;
 import com.idea5.four_cut_photos_map.domain.member.service.MemberService;
 import com.idea5.four_cut_photos_map.domain.memberTitle.dto.response.MemberTitleInfoResp;
 import com.idea5.four_cut_photos_map.domain.memberTitle.entity.MemberTitle;
+import com.idea5.four_cut_photos_map.domain.memberTitle.entity.MemberTitleLog;
+import com.idea5.four_cut_photos_map.domain.memberTitle.repository.MemberTitleLogRepository;
 import com.idea5.four_cut_photos_map.domain.memberTitle.repository.MemberTitleRepository;
+import com.idea5.four_cut_photos_map.global.util.DatabaseCleaner;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,6 +39,9 @@ class MemberTitleServiceTest {
     @Autowired
     private MemberTitleRepository memberTitleRepository;
 
+    @Autowired
+    private MemberTitleLogRepository memberTitleLogRepository;
+
     @BeforeEach
     void init() {
         log.info("---Before init()---");
@@ -45,16 +52,16 @@ class MemberTitleServiceTest {
         memberTitleRepository.save(new MemberTitle("찜 홀릭", "찜 3개 이상 추가", "찜 홀릭 컬러 이미지", "찜 홀릭 흑백 이미지"));
     }
 
-//    @Autowired
-//    DatabaseCleaner databaseCleaner;
-//
-//    @AfterEach
-//    void clear() {
-//        databaseCleaner.execute();
-//    }
+    @Autowired
+    DatabaseCleaner databaseCleaner;
+
+    @AfterEach
+    void clear() {
+        databaseCleaner.execute();
+    }
 
     @Test
-    @DisplayName("1번 회원이 획득하지 않은 2번째 회원 칭호 정보를 조회한다.")
+    @DisplayName("1번 회원이 획득하지 않은 2번 칭호 정보를 조회한다.")
     void t1() {
         // given
         Member member = memberRepository.save(Member.builder().id(1L).nickname("user").build());
@@ -68,6 +75,48 @@ class MemberTitleServiceTest {
                 () -> assertThat(memberTitleInfo.getContent()).isEqualTo("첫번째 리뷰 작성"),
                 () -> assertThat(memberTitleInfo.getImageUrl()).isEqualTo("리뷰 첫 걸음 흑백 이미지"),
                 () -> assertThat(memberTitleInfo.getIsHolding()).isEqualTo(false),
+                () -> assertThat(memberTitleInfo.getIsMain()).isEqualTo(false)
+        );
+    }
+
+    @Test
+    @DisplayName("1번 회원이 획득하고 대표 칭호로 설정된 1번 칭호 정보를 조회한다.")
+    void t2() {
+        // given
+        Member member = memberRepository.save(Member.builder().id(1L).nickname("user").build());
+        Long memberTitleId = 1L;
+        MemberTitle memberTitle = memberTitleRepository.findById(memberTitleId).orElse(null);
+        memberTitleLogRepository.save(new MemberTitleLog(member, memberTitle, true));
+        // when
+        MemberTitleInfoResp memberTitleInfo = memberTitleService.getMemberTitleInfo(memberTitleId, member.getId());
+        // then
+        assertAll(
+                () -> assertThat(memberTitleInfo.getId()).isEqualTo(1L),
+                () -> assertThat(memberTitleInfo.getName()).isEqualTo("뉴비"),
+                () -> assertThat(memberTitleInfo.getContent()).isEqualTo("회원가입"),
+                () -> assertThat(memberTitleInfo.getImageUrl()).isEqualTo("뉴비 컬러 이미지"),
+                () -> assertThat(memberTitleInfo.getIsHolding()).isEqualTo(true),
+                () -> assertThat(memberTitleInfo.getIsMain()).isEqualTo(true)
+        );
+    }
+
+    @Test
+    @DisplayName("1번 회원이 획득하고 대표 칭호로 설정되지 않은 1번 칭호 정보를 조회한다.")
+    void t3() {
+        // given
+        Member member = memberRepository.save(Member.builder().id(1L).nickname("user").build());
+        Long memberTitleId = 1L;
+        MemberTitle memberTitle = memberTitleRepository.findById(memberTitleId).orElse(null);
+        memberTitleLogRepository.save(new MemberTitleLog(member, memberTitle, false));
+        // when
+        MemberTitleInfoResp memberTitleInfo = memberTitleService.getMemberTitleInfo(memberTitleId, member.getId());
+        // then
+        assertAll(
+                () -> assertThat(memberTitleInfo.getId()).isEqualTo(1L),
+                () -> assertThat(memberTitleInfo.getName()).isEqualTo("뉴비"),
+                () -> assertThat(memberTitleInfo.getContent()).isEqualTo("회원가입"),
+                () -> assertThat(memberTitleInfo.getImageUrl()).isEqualTo("뉴비 컬러 이미지"),
+                () -> assertThat(memberTitleInfo.getIsHolding()).isEqualTo(true),
                 () -> assertThat(memberTitleInfo.getIsMain()).isEqualTo(false)
         );
     }
